8d0ffa50c0157889b9dfdcdbaf5b6cd8f954b49d
michelleheh <xiaoyu.he22@gmail.com>
2016-05-26 21:14:00 -0700

    Setup write to user_groups table

--- app/controllers/v1/user_groups_controller.js
+++ app/controllers/v1/user_groups_controller.js
@@ -4,6 +4,8 @@ module.exports = (function() {
 
   const Nodal = require('nodal');
   const UserGroup = Nodal.require('app/models/user_group.js');
+  const User = Nodal.require('app/models/user.js');
+  const Group = Nodal.require('app/models/group.js');
 
   class V1UserGroupsController extends Nodal.Controller {
 
@@ -20,7 +22,7 @@ module.exports = (function() {
     }
 
     show() {
-
+ 
       UserGroup.find(this.params.route.id, (err, model) => {
 
         this.respond(err || model);
@@ -31,9 +33,27 @@ module.exports = (function() {
 
     create() {
 
-      UserGroup.create(this.params.body, (err, model) => {
+      let user_id;
+      let group_id;
+      let user_group;
+      const userName = { name: this.params.body.userName };
+      const groupName = { name: this.params.body.groupName };
 
-        this.respond(err || model);
+      User.findOrCreateBy("name", userName, (err, user) => {
+        if ( err ) { return this.respond(err); } // make sure your handle error
+        user_id = user.get('id');
+
+        Group.findOrCreateBy("name", groupName, (err, group) => {
+          if ( err ) { return this.respond(err); } // every step!!!
+          group_id = group.get('id');
+
+          user_group = { user_id, group_id };
+          
+          UserGroup.create(user_group, (err, userGroup) => {
+            this.respond(err || userGroup);
+          });
+
+        });
 
       });
 
